e3e8b63f1e76e88fdce171bb15711acafa88a3e8
ksy2244 <rlathdus2244@naver.com>
2021-04-08 00:10:29 +0900

    1

---
+++ src/main/java/kr/ac/jbnu/ampm/ksoyeon/KsoyeonApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.ampm.ksoyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsoyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsoyeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/jbnu/ampm/ksoyeon/KsoyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.ampm.ksoyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsoyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
