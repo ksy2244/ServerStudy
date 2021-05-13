ed339ee19bd5730a21184ad15aeb6d8465fec282
ksy2244 <rlathdus2244@naver.com>
2021-05-13 15:05:13 +0900

    Controller 수정

--- src/main/java/kr/ac/jbnu/ampm/ksoyeon/Controller.java
+++ src/main/java/kr/ac/jbnu/ampm/ksoyeon/Controller.java
@@ -3,38 +3,100 @@ package kr.ac.jbnu.ampm.ksoyeon;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
+
 public class Controller {
+    // get부터 id로 받는 것 --> type으로 받기, Post,delete
+    // 1. testdb 2.get all 3. get id 4.post 5.delete
+    public static HashMap<String, ArrayList<Map<String, Object>>> testDBHashMap = new HashMap<String, ArrayList<Map<String, Object>>>();
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<?> getAllResponseEntity(HttpServletRequest request) {
+        ResponseEntity<?> responseEntity = null;
+
+        if (!testDBHashMap.isEmpty()) {
+            responseEntity = new ResponseEntity<>(testDBHashMap, HttpStatus.OK);
+
+        } else {
+            responseEntity = new ResponseEntity<>("NOT_FOUND", HttpStatus.NOT_FOUND);
+        }
+
+        return responseEntity;
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<?> responseEntity(HttpServletRequest request, @PathVariable String id){
+    public ResponseEntity<?> getResponseEntity(HttpServletRequest request, @PathVariable String id) {
         ResponseEntity<?> responseEntity = null;
-        Map<String, Object> voMap = null;
 
-        if(id != null && !id.equals("")){
-            voMap = new HashMap<String, Object>();
+        if (!testDBHashMap.isEmpty()) {
+            if (id != null && !id.equals("") && testDBHashMap.containsKey(id)) {
+                responseEntity = new ResponseEntity<>(testDBHashMap.get(id), HttpStatus.OK);
 
-            voMap.put("books",new LinkedHashMap<String, Object>(){{
-                put("book3",new HashMap<String, Object>(){{
-                    put("name","디지털공학개론");
-                }});
-                put("book2","소프트웨어공학개론");
-                put("book1","마션");
-            }});
-            voMap.put("name","김소연");
-            voMap.put("age","22");
+            } else {
+                responseEntity = new ResponseEntity<>("NOT_FOUND", HttpStatus.NOT_FOUND);
+            }
 
-            responseEntity = new ResponseEntity<>(voMap, HttpStatus.OK);
-        }else{
-            responseEntity = new ResponseEntity<>("NOT_FOUND",HttpStatus.NOT_FOUND);
+        } else {
+            responseEntity = new ResponseEntity<>("NOT_FOUND", HttpStatus.NOT_FOUND);
+        }
+
+        return responseEntity;
+    }
+
+    @RequestMapping(value = "/post/{id}", method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<?> postResponseEntity(HttpServletRequest request, @PathVariable String id, @RequestBody Map<String, Object> requestMap) {
+        ResponseEntity<?> responseEntity = null;
+        ArrayList<Map<String, Object>> postValueArrayList = null;
+
+        if (id != null && !id.equals("")) {
+
+            if(testDBHashMap.containsKey(id)) {
+                postValueArrayList = testDBHashMap.get(id);
+            } else {
+                postValueArrayList = new ArrayList<Map<String, Object>>();
+            }
+
+            postValueArrayList.add(requestMap);
+
+            if (testDBHashMap.containsKey(id)) {
+                testDBHashMap.replace(id, postValueArrayList);
+            } else {
+                testDBHashMap.put(id, postValueArrayList);
+            }
+
+            responseEntity = new ResponseEntity<>(requestMap, HttpStatus.OK);
+
+        } else {
+            responseEntity = new ResponseEntity<>("NOT_CONTAIN", HttpStatus.BAD_REQUEST);
+        }
+
+        return responseEntity;
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<?> deleteResponseEntity(HttpServletRequest request, @PathVariable String id) {
+        ResponseEntity<?> responseEntity = null;
+
+        if (id != null && !id.equals("")) {
+            if (testDBHashMap.containsKey(id)) {
+                testDBHashMap.remove(id);
+                responseEntity = new ResponseEntity<>("", HttpStatus.OK);
+
+            } else {
+                responseEntity = new ResponseEntity<>("NOT_CONTAIN", HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            responseEntity = new ResponseEntity<>("NOT_CONTAIN", HttpStatus.BAD_REQUEST);
         }
         return responseEntity;
     }
 }
-
